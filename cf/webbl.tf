973724b2994d741f89b276982e35feffc8ccf6d9
Andy Liu <andyliuliming@outlook.com>
2018-05-21 22:47:42 +0800

    http/https backend rule for cf load balancer.

--- cf/webbl.tf
+++ cf/webbl.tf
@@ -19,15 +19,35 @@ resource "azurerm_lb" "cf-balancer" {
 }
 
 resource "azurerm_lb_backend_address_pool" "cf-balancer-backend-pool" {
-  name                = "${var.prefix}cf-backend-pool"
+  name                = "${var.prefix}-cf-backend-pool"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   loadbalancer_id     = "${azurerm_lb.cf-balancer.id}"
 }
 
 resource "azurerm_lb_probe" "web-https-probe" {
-  name                = "web-https-probe"
+  name                = "${var.prefix}-web-https-probe"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   loadbalancer_id     = "${azurerm_lb.cf-balancer.id}"
   protocol            = "TCP"
   port                = 443
+}
+
+resource "azurerm_lb_rule" "cf-balancer-rule-https" {
+  resource_group_name            = "${azurerm_resource_group.rg.name}"
+  loadbalancer_id                = "${azurerm_lb.cf-balancer.id}"
+  name                           = "${var.prefix}-https-rule"
+  protocol                       = "Tcp"
+  frontend_port                  = 443
+  backend_port                   = 443
+  frontend_ip_configuration_name = "${azurerm_public_ip.cf-balancer-ip.name}"
+}
+
+resource "azurerm_lb_rule" "cf-balancer-rule-http" {
+  resource_group_name            = "${azurerm_resource_group.rg.name}"
+  loadbalancer_id                = "${azurerm_lb.cf-balancer.id}"
+  name                           = "${var.prefix}-http-rule"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "${azurerm_public_ip.cf-balancer-ip.name}"
 }
