1065cbdf49700f94fbc3cf786ec284bafdd3bcd0
Andy Liu <andyliuliming@outlook.com>
2018-09-16 00:58:41 +0800

    cfcr deployment preparation.

--- cfcr/resource_group.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "${module.variables.resource-group-name}"
-  location = "${var.location}"
-
-  tags {
-    environment = "${module.variables.environment-tag}"
-  }
-}
