3d59f6068b2bf93437335d80320bc32bda33ebb1
Andy Liu <andyliuliming@outlook.com>
2018-09-17 17:19:07 +0800

    fix.

--- cf-lite-bbl/terraform/cf_network.tf
+++ cf-lite-bbl/terraform/cf_network.tf
@@ -6,7 +6,7 @@ resource "azurerm_subnet" "cf-subnet" {
   name                 = "${var.env_id}-cf-sn"
   resource_group_name  = "${azurerm_resource_group.bosh.name}"
   depends_on           = ["azurerm_virtual_network.bosh"]
-  virtssual_network_name = "${azurerm_virtual_network.bosh.name}"
+  virtual_network_name = "${azurerm_virtual_network.bosh.name}"
   address_prefix       = "${cidrsubnet(var.network_cidr, 4, 1)}" //network_cidr = 10.0.0.0/16 => 10.0.16.0/20
 }
 
