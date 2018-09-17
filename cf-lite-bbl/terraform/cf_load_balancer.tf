ca9e150b40845ac4e0ebb90dfd3429efe6449b71
Andy Liu <andyliuliming@outlook.com>
2018-09-17 23:38:25 +0800

    fix.

--- cf-lite-bbl/terraform/cf_load_balancer.tf
+++ cf-lite-bbl/terraform/cf_load_balancer.tf
@@ -1,7 +1,7 @@
 resource "azurerm_public_ip" "cf-balancer-ip" {
   name                         = "${var.env_id}-cf-lb-ip"
   location                     = "${var.region}"
-  resource_group_name          = "${azurerm_resource_group.bosh.name}"
+  resource_group_name          = "${data.azurerm_resource_group.bosh.name}"
 #   depends_on                   = ["azurerm_resource_group.bosh"]
   public_ip_address_allocation = "static"
   sku                          = "Standard"
@@ -11,7 +11,7 @@ resource "azurerm_lb" "cf-balancer" {
   name                = "${var.env_id}-cf-lb"
   location            = "${var.region}"
   sku                 = "Standard"
-  resource_group_name = "${azurerm_resource_group.bosh.name}"
+  resource_group_name = "${data.azurerm_resource_group.bosh.name}"
 #   depends_on          = ["azurerm_public_ip.cf-balancer-ip"]
 
   frontend_ip_configuration {
@@ -22,14 +22,14 @@ resource "azurerm_lb" "cf-balancer" {
 
 resource "azurerm_lb_backend_address_pool" "cf-balancer-backend-pool" {
   name                = "${var.env_id}-cf-backend-pool"
-  resource_group_name = "${azurerm_resource_group.bosh.name}"
+  resource_group_name = "${data.azurerm_resource_group.bosh.name}"
 #   depends_on          = ["azurerm_lb.cf-balancer"]
   loadbalancer_id     = "${azurerm_lb.cf-balancer.id}"
 }
 
 resource "azurerm_lb_probe" "health-probe" {
   name                = "${var.env_id}-health-probe"
-  resource_group_name = "${azurerm_resource_group.bosh.name}"
+  resource_group_name = "${data.azurerm_resource_group.bosh.name}"
 #   depends_on          = ["azurerm_lb.cf-balancer"]
   loadbalancer_id     = "${azurerm_lb.cf-balancer.id}"
   protocol            = "HTTP"
@@ -41,7 +41,7 @@ resource "azurerm_lb_probe" "health-probe" {
 
 resource "azurerm_lb_rule" "cf-balancer-rule-https" {
   name                           = "${var.env_id}-https-rule"
-  resource_group_name            = "${azurerm_resource_group.bosh.name}"
+  resource_group_name            = "${data.azurerm_resource_group.bosh.name}"
 #   depends_on                     = ["azurerm_lb_probe.health-probe", "azurerm_lb_backend_address_pool.cf-balancer-backend-pool"]
   loadbalancer_id                = "${azurerm_lb.cf-balancer.id}"
   protocol                       = "Tcp"
@@ -54,7 +54,7 @@ resource "azurerm_lb_rule" "cf-balancer-rule-https" {
 
 resource "azurerm_lb_rule" "cf-balancer-rule-http" {
   name                           = "${var.env_id}-http-rule"
-  resource_group_name            = "${azurerm_resource_group.bosh.name}"
+  resource_group_name            = "${data.azurerm_resource_group.bosh.name}"
 #   depends_on                     = ["azurerm_lb_probe.health-probe", "azurerm_lb_backend_address_pool.cf-balancer-backend-pool"]
   loadbalancer_id                = "${azurerm_lb.cf-balancer.id}"
   protocol                       = "Tcp"
