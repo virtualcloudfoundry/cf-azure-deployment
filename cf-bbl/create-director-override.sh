00ccfc4506cc6bac4fc5c8f32eb543e0afd2db7a
Andy Liu <andyliuliming@outlook.com>
2018-09-18 14:47:44 +0800

    fix.

--- cf-bbl/create-director-override.sh
+++ cf-bbl/create-director-override.sh
@@ -5,12 +5,11 @@ bosh create-env \
   --vars-store  ${BBL_STATE_DIR}/vars/director-vars-store.yml \
   --vars-file  ${BBL_STATE_DIR}/vars/director-vars-file.yml \
   -o  ${BBL_STATE_DIR}/bosh-deployment/azure/cpi.yml \
+  -o  ${BBL_STATE_DIR}/bosh-deployment/azure/use-managed-disks.yml \
   -o  ${BBL_STATE_DIR}/bosh-deployment/jumpbox-user.yml \
   -o  ${BBL_STATE_DIR}/bosh-deployment/uaa.yml \
   -o  ${BBL_STATE_DIR}/bosh-deployment/credhub.yml \
   -o  ${BBL_STATE_DIR}/common-manifests/small_vm_director.yml \
-  -o  ${BBL_STATE_DIR}/common-manifests/use_managed_disk.yml \
-  -o  ${BBL_STATE_DIR}/common-manifests/custom_stemcell.yml \
   -o  ${BBL_STATE_DIR}/common-manifests/custom_cpi.yml \
   -v  subscription_id="${BBL_AZURE_SUBSCRIPTION_ID}" \
   -v  client_id="${BBL_AZURE_CLIENT_ID}" \
