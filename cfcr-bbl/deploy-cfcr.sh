090ca5e581686a285b7bc1e6e5243c215bb2abb0
Andy Liu <andyliuliming@outlook.com>
2018-09-20 21:41:21 +0800

    fi

--- cfcr-bbl/deploy-cfcr.sh
+++ cfcr-bbl/deploy-cfcr.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 bosh upload-stemcell --sha1=073edfb9315aa318c24c0b9968dd9e30c73fe409 https://opensourcerelease.blob.core.windows.net/releases/bosh-stemcell-6666.66-azure-hyperv-ubuntu-xenial-go_agent.tgz
 
-bosh -n deploy -d azurecfcr ./kubo-deployment/manifests/cfcr.yml \
+# create one cloud config specific to the deployment
+export deployment_name="azurecfcr"
+bosh update-config --name ${deployment_name} \
+   ./kubo-deployment/manifests/cloud-config/iaas/azure/use-vm-extensions.yml \
+   --type cloud \
+   --vars-file=./vars/director-vars-file.yml \
+   -v deployment_name=${deployment_name}
+
+bosh -n deploy -d ${deployment_name} ./kubo-deployment/manifests/cfcr.yml \
     --vars-file=./vars/director-vars-file.yml \
     -o ./kubo-deployment/manifests/ops-files/iaas/azure/cloud-provider.yml \
     -o ./kubo-deployment/manifests/ops-files/misc/single-master.yml \
@@ -11,7 +19,7 @@ bosh -n deploy -d azurecfcr ./kubo-deployment/manifests/cfcr.yml \
     -o ./kubo-deployment/customize_kubo_release.yml \
     -o ./kubo-deployment/manifests/ops-files/misc/small-vm.yml \
     -o ./kubo-deployment/manifests/ops-files/iaas/azure/subnet.yml \
-    -v deployment_name=azurecfcr \
+    -v deployment_name=${deployment_name} \
     -v subscription_id="${BBL_AZURE_SUBSCRIPTION_ID}" \
     -v client_id="${BBL_AZURE_CLIENT_ID}" \
     -v client_secret="${BBL_AZURE_CLIENT_SECRET}" \
