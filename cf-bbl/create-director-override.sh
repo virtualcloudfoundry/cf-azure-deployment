80e009de57837ff71b57eb41f3df2454755e0d56
Andy Liu <andyliuliming@outlook.com>
2018-09-20 13:57:31 +0800

    fix.

--- cf-bbl/create-director-override.sh
+++ cf-bbl/create-director-override.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+echo "create-env:"
+bosh interpolate \
+  ${BBL_STATE_DIR}/bosh-deployment/bosh.yml \
+  --vars-store  ${BBL_STATE_DIR}/vars/director-vars-store.yml \
+  --vars-file  ${BBL_STATE_DIR}/vars/director-vars-file.yml \
+  -o  ${BBL_STATE_DIR}/bosh-deployment/azure/cpi.yml \
+  -o  ${BBL_STATE_DIR}/bosh-deployment/azure/use-managed-disks.yml \
+  -o  ${BBL_STATE_DIR}/bosh-deployment/jumpbox-user.yml \
+  -o  ${BBL_STATE_DIR}/bosh-deployment/uaa.yml \
+  -o  ${BBL_STATE_DIR}/bosh-deployment/credhub.yml \
+  -o  ${BBL_STATE_DIR}/common-manifests/vm_director.yml \
+  -o  ${BBL_STATE_DIR}/common-manifests/custom_cpi.yml \
+  -v  subscription_id="${BBL_AZURE_SUBSCRIPTION_ID}" \
+  -v  client_id="${BBL_AZURE_CLIENT_ID}" \
+  -v  client_secret="${BBL_AZURE_CLIENT_SECRET}" \
+  -v  tenant_id="${BBL_AZURE_TENANT_ID}"
+
 bosh create-env \
   ${BBL_STATE_DIR}/bosh-deployment/bosh.yml \
   --state  ${BBL_STATE_DIR}/vars/bosh-state.json \
@@ -9,9 +26,8 @@ bosh create-env \
   -o  ${BBL_STATE_DIR}/bosh-deployment/jumpbox-user.yml \
   -o  ${BBL_STATE_DIR}/bosh-deployment/uaa.yml \
   -o  ${BBL_STATE_DIR}/bosh-deployment/credhub.yml \
-  -o  ${BBL_STATE_DIR}/common-manifests/small_vm_director.yml \
+  -o  ${BBL_STATE_DIR}/common-manifests/vm_director.yml \
   -o  ${BBL_STATE_DIR}/common-manifests/custom_cpi.yml \
-  -o  ${BBL_STATE_DIR}/common-manifests/use_root_disk_as_ephemeral_director.yml \
   -v  subscription_id="${BBL_AZURE_SUBSCRIPTION_ID}" \
   -v  client_id="${BBL_AZURE_CLIENT_ID}" \
   -v  client_secret="${BBL_AZURE_CLIENT_SECRET}" \
--- cf-bbl/create-jumpbox-override.sh
+++ cf-bbl/create-jumpbox-override.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
+echo "create-env:"
+bosh interpolate \
+  ${BBL_STATE_DIR}/jumpbox-deployment/jumpbox.yml \
+  --vars-store  ${BBL_STATE_DIR}/vars/jumpbox-vars-store.yml \
+  --vars-file  ${BBL_STATE_DIR}/vars/jumpbox-vars-file.yml \
+  -o  ${BBL_STATE_DIR}/jumpbox-deployment/azure/cpi.yml \
+  -o  ${BBL_STATE_DIR}/common-manifests/vm_director.yml \
+  -o  ${BBL_STATE_DIR}/common-manifests/use_managed_disk.yml \
+  -o  ${BBL_STATE_DIR}/common-manifests/custom_cpi.yml \
+  -v  subscription_id="${BBL_AZURE_SUBSCRIPTION_ID}" \
+  -v  client_id="${BBL_AZURE_CLIENT_ID}" \
+  -v  client_secret="${BBL_AZURE_CLIENT_SECRET}" \
+  -v  tenant_id="${BBL_AZURE_TENANT_ID}"
+
 bosh create-env \
   ${BBL_STATE_DIR}/jumpbox-deployment/jumpbox.yml \
   --state  ${BBL_STATE_DIR}/vars/jumpbox-state.json \
   --vars-store  ${BBL_STATE_DIR}/vars/jumpbox-vars-store.yml \
   --vars-file  ${BBL_STATE_DIR}/vars/jumpbox-vars-file.yml \
   -o  ${BBL_STATE_DIR}/jumpbox-deployment/azure/cpi.yml \
-  -o  ${BBL_STATE_DIR}/common-manifests/small_vm_director.yml \
+  -o  ${BBL_STATE_DIR}/common-manifests/vm_director.yml \
   -o  ${BBL_STATE_DIR}/common-manifests/use_managed_disk.yml \
   -o  ${BBL_STATE_DIR}/common-manifests/custom_cpi.yml \
-  -o  ${BBL_STATE_DIR}/common-manifests/use_root_disk_as_ephemeral_jumpbox.yml \
   -v  subscription_id="${BBL_AZURE_SUBSCRIPTION_ID}" \
   -v  client_id="${BBL_AZURE_CLIENT_ID}" \
   -v  client_secret="${BBL_AZURE_CLIENT_SECRET}" \
-  -v  tenant_id="${BBL_AZURE_TENANT_ID}" 
+  -v  tenant_id="${BBL_AZURE_TENANT_ID}"
