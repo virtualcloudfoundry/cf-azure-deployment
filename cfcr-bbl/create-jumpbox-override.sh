fbeafa29288de9eec87384c0cafec9d8cc4ca724
Andy Liu <andyliuliming@outlook.com>
2018-09-28 16:19:11 +0800

    fix wrongly using vm_director.yml for jumpbox.

--- cfcr-bbl/create-jumpbox-override.sh
+++ cfcr-bbl/create-jumpbox-override.sh
@@ -5,7 +5,7 @@ bosh interpolate \
   --vars-store  ${BBL_STATE_DIR}/vars/jumpbox-vars-store.yml \
   --vars-file  ${BBL_STATE_DIR}/vars/jumpbox-vars-file.yml \
   -o  ${BBL_STATE_DIR}/jumpbox-deployment/azure/cpi.yml \
-  -o  ${BBL_STATE_DIR}/common-manifests/vm_director.yml \
+  -o  ${BBL_STATE_DIR}/common-manifests/vm_jumpbox.yml \
   -o  ${BBL_STATE_DIR}/common-manifests/use_managed_disk.yml \
   -o  ${BBL_STATE_DIR}/common-manifests/custom_cpi.yml \
   -v  subscription_id="${BBL_AZURE_SUBSCRIPTION_ID}" \
@@ -19,7 +19,7 @@ bosh create-env \
   --vars-store  ${BBL_STATE_DIR}/vars/jumpbox-vars-store.yml \
   --vars-file  ${BBL_STATE_DIR}/vars/jumpbox-vars-file.yml \
   -o  ${BBL_STATE_DIR}/jumpbox-deployment/azure/cpi.yml \
-  -o  ${BBL_STATE_DIR}/common-manifests/vm_director.yml \
+  -o  ${BBL_STATE_DIR}/common-manifests/vm_jumpbox.yml \
   -o  ${BBL_STATE_DIR}/common-manifests/use_managed_disk.yml \
   -o  ${BBL_STATE_DIR}/common-manifests/custom_cpi.yml \
   -v  subscription_id="${BBL_AZURE_SUBSCRIPTION_ID}" \
